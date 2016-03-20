857e8941f66d7c5cb5f15fac7f2c0274ea2d627d
hhmhywzr <eazow@163.com>
2016-03-20 15:44:00 +0800

    update 70 Climbing Stairs

--- 70_climbing_stairs.c
+++ 70_climbing_stairs.c
@@ -1,18 +1,36 @@
 #include <assert.h>
+#include <stdio.h>
 
+
+/**
+ * f(2) = f(0) + f(1)
+ * f(3) = f(1) + f(2)
+ * f(4) = f(2) + f(3) 
+ * f(5) = f(3) + f(4)
+ */
 int climbStairs(int n) {
-    if(n<=3)
-        return n;
-    return climbStairs(n-1)+climbStairs(n-2);
+/*
+	if(n < 3) 
+		return n;
+	return climbStairs(n-1) + climbStairs(n-2);
+*/
+	int pre1 = 0, pre2 = 1, result = 1;
+
+	while(n-- > 0) {
+		result = pre1 + pre2;
+		pre1 = pre2;
+		pre2 = result;
+	}
+
+	return result;
 }
 
 int main(int argc, char **argv) {
     assert(climbStairs(1) == 1);
     assert(climbStairs(2) == 2);
     assert(climbStairs(3) == 3);
-
-    climbStairs(50);
-
+	assert(climbStairs(4) == 5);
+	assert(climbStairs(5) == 8);
 
     return 0;
 }
