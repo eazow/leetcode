5dc7c60c52535082b9f8a59643522ca3ec2aff73
hhmhywzr <eazow@163.com>
2016-03-28 23:08:41 +0800

    add 107 Binary Tree Level Order Traversal II

---
+++ 107_binary_tree_level_order_traversal_2.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct Queue {
+    struct TreeNode **node;
+    int head;
+    int tail;
+
+}
+
+int **levelOrderBottom(struct TreeNode *root, int **columnSizes, int *returnSize) {
+
+}
+
+void traversal(root) {
+    Queue *queue = malloc(sizeof(Queue));
+    if(root) {
+        Queue->push(root);
+        Queue->pop(root);
+
+    }
+
+}
