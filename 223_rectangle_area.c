97774ed1207fbd6ae75ece7ce14c5c6f3de55dc1
hhmhywzr <eazow@163.com>
2016-04-20 22:38:40 +0800

    Update 223 Rectangle Area

--- 223_rectangle_area.c
+++ 223_rectangle_area.c
@@ -1,4 +1,6 @@
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void sort(int *nums, int length) {
     int i = 0, j = 0;
@@ -15,6 +17,15 @@ void sort(int *nums, int length) {
 }
 
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int area1 = (C-A)*(D-B);
+    int area2 = (G-E)*(H-F);
+    if(C<=E || D<=F || A>=G || B>=H)
+        return area1 + area2;
+    if(A>=E && B>=F && C<=G && D<=H)
+        return area2;
+    if(E>=A && F>=B && G>=C && H>=D)
+        return area1;
+
     int nums1[4] = {A, C, E, G};
     sort(nums1, 4);
     int length = nums1[2] - nums1[1];
@@ -22,11 +33,11 @@ int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
     sort(nums2, 4);
     int width = nums2[2] - nums2[1];
 
-    return length * width;
+    return area1+area2-length * width;
 }
 
 int main() {
-    assert(computeArea(-3, 0, 3, 5, 0, -1, 9, 2) == 6);
+    assert(computeArea(-3, 0, 3, 4, 0, -1, 9, 2) == 45);
 
     return 0;
 }
