84caac49c4fcd2cf6a4a9ee06becfda986b793b2
hhmhywzr <eazow@163.com>
2016-04-20 17:54:51 +0800

    Update 88 Merge Sorted Array

--- 88_merge_sorted_array.c
+++ 88_merge_sorted_array.c
@@ -2,14 +2,24 @@
 
 void merge(int* nums1, int m, int* nums2, int n) {
     int index = m+n-1;
-    
+    m--;
+    n--;
     while(index >= 0) {
-        if(n <= 0 || nums1[m-1] > nums2[n-1])
-            nums1[index--] = nums1[--m];
-        else 
-            nums2[index--] = nums2[--n];
+        if((m >= 0 && nums1[m] > nums2[n]) || n < 0)
+            nums1[index--] = nums1[m--];
+        else
+            nums1[index--] = nums2[n--];
+    }
 }
 
 int main() {
+    int num1[9] = {1,3,5,7,9,0,0,0,0};
+    int num2[4] = {2,4,6,8};
+
+    merge(num1, 5, num2, 4);
+
+    assert(num1[0] == 1);
+    assert(num1[1] == 2);
+
     return 0;
 }
