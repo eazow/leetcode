8c942999da60f0d804bb664f8116f6bee7673203
hhmhywzr <eazow@163.com>
2016-04-23 23:02:55 +0800

    Update 299 Bulls and Cows

--- 299_bulls_and_cows.c
+++ 299_bulls_and_cows.c
@@ -1,6 +1,27 @@
 #include <assert.h>
 
+#define min(A,B) ((A)<(B)?(A):(B))
+
 char* getHint(char *secret, char* guess) {
+    int i = 0;
+    int aNum = 0;
+    int hashSecret = (int *)malloc(sizeof(int)*10);
+    int hashGuess = (int *)malloc(sizeof(int)*10);
+    memset(hashSecret, 0, 10);
+    memset(hashGuess, 0, 10);
+    for(i = 0; secret[i] != '\0'; i++) {
+        if(secret[i] == guess[i])
+            aNum++;
+        else {
+            hashSecret[secret[i]]++;
+            hashGuess[guess[i]]++;
+        }
+    }
+    int bNum = 0;
+    for(i = 0; i < 10; i++) {
+        bNum += min(hashSecret[i], hashGuess[i]);
+    }
+
 
 }
 
