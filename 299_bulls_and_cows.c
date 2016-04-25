132481e3b07fcc85885b2daf2962f102d4bfc3bc
hhmhywzr <eazow@163.com>
2016-04-25 18:59:42 +0800

    Update 299 Bulls and Cows

--- 299_bulls_and_cows.c
+++ 299_bulls_and_cows.c
@@ -1,17 +1,15 @@
 #include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define min(A,B) ((A)<(B)?(A):(B))
 
 char* getHint(char *secret, char* guess) {
     int i = 0;
     int aNum = 0;
-    int *hashSecret = (int *)malloc(sizeof(int)*10);
-    int *hashGuess = (int *)malloc(sizeof(int)*10);
-    memset(hashSecret, 0, 10);
-    memset(hashGuess, 0, 10);
+    int hashSecret[10] = {0};
+    int hashGuess[10] = {0};
     for(i = 0; secret[i] != '\0'; i++) {
         if(secret[i] == guess[i])
             aNum++;
@@ -25,13 +23,17 @@ char* getHint(char *secret, char* guess) {
         bNum += min(hashSecret[i], hashGuess[i]);
     }
     
-    char s[1000];
+    char *s = (char *)malloc(sizeof(char) * 100);
+    sprintf(s, "%dA%dB", aNum, bNum);
 
-    return (char *)s;
+    return s;
 }
 
 int main() {
     char *s = getHint("1807", "7810");
+
+    assert(s[0] == '1');
+    assert(s[2] == '3');
 
     return 0;
 }
