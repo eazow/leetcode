5085520fd939d1ac29a7cf6262bef302c8fe57e0
hhmhywzr <eazow@163.com>
2016-04-26 23:12:42 +0800

    Add 19 Remove Nth Node From End of List

---
+++ 19_remove_nth_node_from_end_of_list.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode* p1 = head;
+    struct ListNode* p2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    p2->next = head;
+
+    for(; p1 != NULL; p1=p1->next, n--) {
+        if(n < 0)
+            p2 = p2->next;
+    }
+    struct ListNode* temp = p2->next;
+    p2->next = p2->next->next;
+    free(temp);
+
+    return p2->next;
+}
+
+int main() {
+    struct ListNode* head = (struct ListNode *)malloc(sizeof(struct ListNode*));
+    head->val = 1;
+    struct ListNode* node = (struct ListNode *)malloc(sizeof(struct ListNode*));
+    node->val = 2;
+    head->next = node;
+    node->next = NULL;
+
+    assert(removeNthFromEnd(head, 2) == node);
+
+    return 0;
+}
