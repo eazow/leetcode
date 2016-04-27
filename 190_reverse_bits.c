c73c1491bf5c9a5921fa52374bb578db1554a446
hhmhywzr <eazow@163.com>
2016-04-27 23:17:11 +0800

    Update 190 Reverse Bits

--- 190_reverse_bits.c
+++ 190_reverse_bits.c
@@ -1,7 +1,14 @@
 #include <assert.h>
+#include <stdint.h>
 
 uint32_t reverseBits(uint32_t n) {
-
+    uint32_t m = 0;
+    int i = 0;
+    for(i = 0; i < 32; i++,n>>=1) {
+        m = m << 1;
+        m = m | (n & 1);
+    }
+    return m;
 }
 
 int main() {
