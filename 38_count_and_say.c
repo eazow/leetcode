9eb20b6480063a1c6b9180fd815c934fc3bd1a48
hhmhywzr <eazow@163.com>
2016-04-30 23:16:33 +0800

    Update 38 Count and Say

--- 38_count_and_say.c
+++ 38_count_and_say.c
@@ -1,8 +1,25 @@
 #include <assert.h>
+#include <string.h>
 
 char* countAndSay(int n) {
     if(n == 1)
         return "1";
+    char *s = "1";
+    int i = 0;
+    int strLen = strlen(s);
+    int count = 0;
+    char pre = s[0];
+    char cur;
+    char* statistic = (char*)malloc(sizeof(char) * (2 * strlen + 2));
+    while(++s) {
+        cur = s;
+        if(cur == pre)
+            count++;
+        else {
+            pre = *s;
+        }
+    }
+
 }
 
 int main() {
