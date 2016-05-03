945cd94bc38383903c59c4ece65126f17392becd
hhmhywzr <eazow@163.com>
2016-05-03 23:11:23 +0800

    Add 203 Remove Linked List Elements

---
+++ 203_remove_linked_list_elements.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    
+
+}
+
+int main() {
+    
+    return 0;
+}
