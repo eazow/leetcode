aef3d6cf8ae2c2168f3fe2342b53a2746b746fee
hhmhywzr <eazow@163.com>
2016-05-05 23:32:59 +0800

    Update 203 Remove Linked List Elements

--- 203_remove_linked_list_elements.c
+++ 203_remove_linked_list_elements.c
@@ -7,11 +7,27 @@ struct ListNode {
 };
 
 struct ListNode* removeElements(struct ListNode* head, int val) {
-    
-
-}
+    struct ListNode *pHead = (struct ListNode *)malloc(sizeof(struct ListNode));
+    pHead->next = head;
+    struct ListNode *pre, *cur;
+    for(pre=pHead,cur=head; cur!=NULL; cur=cur->next) {
+        if(cur->val == val)
+            pre->next = cur->next;
+        else
+            pre = cur;
+    }
+    return pHead->next;
+}       
 
 int main() {
+    struct ListNode* node1 = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    node1->val = 1;
+    struct ListNode* node2 = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    node2->val = 2;
+    node1->next = node2;
+    node2->next = NULL;
+
+    assert(removeElements(node1, 1) == node2);
     
     return 0;
 }
