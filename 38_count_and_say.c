db2ef8ee4f7a5c41264db3bc5ecfefcdfeaffc74
hhmhywzr <eazow@163.com>
2016-05-05 20:03:25 +0800

    Update 38 Count and Say

--- 38_count_and_say.c
+++ 38_count_and_say.c
@@ -6,33 +6,43 @@
 char* countAndSay(int n) {
     if(n == 1)
         return "1";
-    char *s = "1";
-    int i = 0;
-    int strLen = strlen(s);
-    int count = 0;
-    char pre = s[0];
-    char cur;
-    char* statistic = (char*)malloc(sizeof(char) * (2 * strLen + 2));
-    int j = 0;
-    for(i = 0; i < strLen; i++) {
-        cur = s[i];
-        if(cur == pre)
-            count++;
-        else {
-            pre = s[i];
-            statistic[j++] = '0' + count;
-            statistic[j++] = '0' + pre;
-        }
-    }
-    statistic[j] = '\0';
 
-    return "1";
+    int nIndex = 1;
+    char *s = "1";
+    char *statistic;
+    for(; nIndex < n; nIndex++) {
+        int i = 0;
+        int strLen = strlen(s);
+        int count = 0;
+        char pre = s[0];
+        char cur;
+        statistic = (char*)malloc(sizeof(char) * (2 * strLen + 2));
+        int j = 0;
+        for(i = 0; i < strLen; i++) {
+            cur = s[i];
+            if(cur == pre)
+                count++;
+            else {
+                statistic[j++] = '0' + count;
+                statistic[j++] = pre;
+                pre = s[i];
+                count = 1;
+            }
+        }
+        if(count > 0) {
+            statistic[j++] = '0' + count;
+            statistic[j++] = cur;
+        }
+        statistic[j] = '\0';
+        s = statistic;
+    }
+    return statistic;
 }
 
 int main() {
     assert(countAndSay(1) == "1");
-
-    countAndSay(2);
+    assert(strcmp(countAndSay(2),"11")==0);
+    assert(strcmp(countAndSay(10), "13211311123113112211")==0);
 
     return 0;
 }
