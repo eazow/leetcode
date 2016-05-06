8304a6fc6ce18e465f07688ff41eca8af35e3028
hhmhywzr <eazow@163.com>
2016-05-06 23:54:08 +0800

    Add 257 Binary Tree Paths

---
+++ 257_binary_tree_paths.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** binaryTreePaths(struct TreeNode* root, int* returnSize) {
+
+    return NULL;
+}
+
+int main() {
+
+    return 0;
+}
