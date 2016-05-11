f16436cbb3eb2f5cdd372d242571f0dacbad7e4a
hhmhywzr <eazow@163.com>
2016-05-11 10:12:00 +0800

    Add 14 Longest Common Prefix

---
+++ 14_longest_common_prefix.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+char *longestCommonPrefix(char** strs, int strSize) {
+    if(strsSize <= 0) 
+        return NULL;
+    char *str = strs[0];
+    int max = len(str);
+
+}
+
+int main() {
+    
+
+
+    return 0;
+}
