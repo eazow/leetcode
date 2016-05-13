50912367865d4df8294aef0709bfdf97a163ab60
hhmhywzr <eazow@163.com>
2016-05-13 23:23:30 +0800

    Update 234 Palindrome Linked List

--- 234_palindrome_linked_list.c
+++ 234_palindrome_linked_list.c
@@ -6,7 +6,15 @@ struct ListNode {
 };
 
 int isPalindrome(struct ListNode* head) {
+    struct ListNode *slow=head, *fast=head;
+    if(head==NULL || head->next==NULL)
+        return 1;
 
+
+    while(fast!=NULL&fast->next!=NULL;) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
 }
 
 int main() {
