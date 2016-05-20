dd6ed378a0a758f01e6b9f8d00700a4d0533e3fd
hhmhywzr <eazow@163.com>
2016-05-20 23:27:52 +0800

    Update 67 Add Binary

--- 67_add_binary.c
+++ 67_add_binary.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int binaryToDecimal(char* str) {
     int strLen = strlen(str);
@@ -33,12 +34,31 @@ char* decimalToBinary(int num) {
 }
 
 char* addBinary(char* a, char* b) {
-    return decimalToBinary(binaryToDecimal(a)+binaryToDecimal(b));
+//  return decimalToBinary(binaryToDecimal(a)+binaryToDecimal(b));
+
+    int lenA = strlen(a);
+    int lenB = strlen(b);
+    int len = lenA>lenB?lenA:lenB;
+    char *sum = (char *)malloc(sizeof(char) * (len+2));
+    sum[len+1] = '\0';
+    int bitSum = 0;
+    while(len >= 0) {
+        if(lenA > 0)
+            bitSum += a[--lenA]-'0';
+        if(lenB > 0)
+            bitSum += b[--lenB]-'0';
+        sum[len--] = (bitSum&0x01)+'0';
+        bitSum >>= 1;
+    }
+    if(sum[0]!='0')
+        return sum;
+    return &sum[1];
 }
 
 int main() {
-    char *sum = addBinary("11" ,"1");
-    assert(strcmp(sum, "100") == 0);
+    assert(strcmp(addBinary("11", "1"), "100") == 0);
+    assert(strcmp(addBinary("100", "1"), "101") == 0);
+    assert(strcmp(addBinary("111111111100000000001111111111000000000011111111110000000000", "1"), "111111111100000000001111111111000000000011111111110000000001") == 0);
 
     return 0;
 }
