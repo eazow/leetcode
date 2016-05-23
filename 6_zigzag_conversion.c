1169ce21e0c908a62d3fd1ccc0367aa356373a36
hhmhywzr <eazow@163.com>
2016-05-23 21:45:13 +0800

    Update 6 ZigZag Conversion

--- 6_zigzag_conversion.c
+++ 6_zigzag_conversion.c
@@ -3,33 +3,42 @@
 #include <string.h>
 #include <stdio.h>
 
+char* filter(char* s, int len) {
+    int i = 0;
+    char* str = (char *)malloc(sizeof(char)*(len+1));
+    int strI = 0;
+    for(i = 0; strI < len; i++) {
+        if(s[i] != '#')
+            str[strI++] = s[i];  
+    }
+    str[strI] = '\0';
+    return str;
+}
+
 char* convert(char* s, int numRows) {
     int len = strlen(s);
     char *statistics = (char *)malloc(sizeof(char)*len*numRows);
     int i = 0;
     for(i = 0; i < len*numRows; i++) {
-        statistics[i] = '-';
+        statistics[i] = '#';
     }
 
     int row, col;
     int oddCol = 1;
     int assendOrder = 0;
     int sI = 0;
-    for(col = 0; col < len; col++) {
+    for(col = 0; col < len && sI < len; col++) {
         //even column
         if(col%2==0) {
             if(assendOrder == 0) {
                 row = 0;
-                while(row < numRows) {
+                while(row < numRows && sI < len)
                     statistics[(row++)*len+col] = s[sI++];
-                }
-                assendOrder = 1;
             }
             else {
                 row = numRows-1;
-                while(row >= 0) {
+                while(row >= 0 && sI < len)
                     statistics[(row--)*len+col] = s[sI++];
-                }
                 assendOrder = 0;
             }
         }
@@ -37,28 +46,24 @@ char* convert(char* s, int numRows) {
         else {
             if(assendOrder == 0) {
                 row = 1;
-                while(row < numRows-1) {
+                while(row < numRows-1 && sI < len)
                     statistics[(row++)*len+col] = s[sI++];
-                }
                 assendOrder = 1;
             }
             else {
                 row = numRows-2;
-                while(row >= 1) {
+                while(row >= 1 && sI < len)
                     statistics[(row--)*len+col] = s[sI++];
-                }
                 assendOrder = 0;
             }
         }
     }
-    for(i = 0; i < len*numRows; i++)
-        printf("%c", statistics[i]);
 
-    return statistics;
+    return filter(statistics, len);
 }
 
 int main() {
-    convert("PAYPALISHIRING", 3);
+    printf("%s\n", convert("PAYPALISHIRING", 3));
 
     return 0;
 }
