3fc5a3a944b1b3f7d7d507ab4d316ef911fffd26
hhmhywzr <eazow@163.com>
2016-05-23 21:32:54 +0800

    Update 204 Count Primes

--- 204_count_primes.c
+++ 204_count_primes.c
@@ -3,15 +3,16 @@
 
 int countPrimes(int n) {
     //0 and 1 are not primes
-    int count = n-1;
-    int* isPrime = (int *)malloc(sizeof(int)*(n+1));
+    int count = n>=2?n-2:0;
+
+    int* isPrime = (int *)malloc(sizeof(int)*n);
     int i = 0;
-    for(i = 0; i <= n; i++) 
+    for(i = 0; i < n; i++) 
         isPrime[i] = 1;
 
     int j = 0;
-    for(i = 2; i <= n; i++) {
-        for(j = i; i*j <= n; j++) {
+    for(i = 2; i*i < n; i++) {
+        for(j = i; i*j < n; j++) {
             if(isPrime[i*j]) {
                 isPrime[i*j] = 0;     
                 count--;
@@ -24,8 +25,7 @@ int countPrimes(int n) {
 
 int main() {
     assert(countPrimes(10) == 4);
-
-    countPrimes(499979);
+    assert(countPrimes(499979) == 41537);
 
     return 0;
 }
