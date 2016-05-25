ca0bb2494873d0b310253713937a401b7006c355
hhmhywzr <eazow@163.com>
2016-05-25 22:02:14 +0800

    Update 168 Excel Sheet Column Title

--- 168_excel_sheet_column_title.c
+++ 168_excel_sheet_column_title.c
@@ -1,22 +1,20 @@
 #include <assert.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 char* convertToTitle(int n) {
-
-    char *title = (char *)malloc(sizeof(char)*32);
-    int i = 31;
+    char *title = (char *)malloc(sizeof(char)*16);
+    int i = 15;
     title[i] = '\0';
     while(n > 0) {
-        title[--i] = (n%26?n%26:26)+'A'-1;
-        n /= 26;
+        title[--i] = (n-1)%26+'A';
+        n = (n-1)/26;
     }
     return title+i;
 }
 
 int main() {
-    convertToTitle(100);
-
+    assert(strcmp(convertToTitle(100), "CV") == 0);
     assert(strcmp(convertToTitle(52), "AZ") == 0);
 
     return 0;
