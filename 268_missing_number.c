af6aa294d6d1f4491f5b4ee1e4f62e7b695ced6f
hhmhywzr <eazow@163.com>
2016-06-02 18:00:35 +0800

    Update 268 Missing Number

--- 268_missing_number.c
+++ 268_missing_number.c
@@ -1,5 +1,18 @@
 #include <assert.h>
 
 int missingNumber(int* nums, int numsSize) {
+    int expectedSum = (1+numsSize)*numsSize/2;
+    int i = 0;
+    int sum = 0;
+    for(i = 0; i < numsSize; i++) {
+        sum += nums[i];
+    }
+    return expectedSum - sum;
+}
 
+int main() {
+    int nums[3] = {0, 1, 3};
+    assert(missingNumber(nums, 3) == 2);
+
+    return 0;
 }
