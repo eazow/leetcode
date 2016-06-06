bd54faf35b57b68f45f8ecdb64ecfc0ed8ccce6a
hhmhywzr <eazow@163.com>
2016-06-06 17:38:28 +0800

    Update 144 Binary Tree Preorder Traversal

--- 144_binary_tree_preorder_traversal.c
+++ 144_binary_tree_preorder_traversal.c
@@ -7,6 +7,10 @@ struct TreeNode {
     struct TreeNode* right;
 };
 
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     if(root == NULL)
         return NULL;
