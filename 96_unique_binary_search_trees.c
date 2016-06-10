bb219c9e274c1948a482050310417e5affdbfcab
hhmhywzr <eazow@163.com>
2016-06-10 16:26:35 +0800

    Update 96 Unique Binary Search Trees

--- 96_unique_binary_search_trees.c
+++ 96_unique_binary_search_trees.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 
 int numTrees(int n) {
-    int* numTrees = (int *)malloc(sizeof(int) * (n+1));
-    if(n = 1)
-        numTrees[n] = 1;
+    int* numTrees = (int *)calloc(n+1, sizeof(int));
+    numTrees[0] = 1;
     int i = 0;
     int j = 0;
-    for(i = 1; i <= n; i++) {
-        for(j = 1; j < i; j++)
-            numTrees[i] = numTrees[j]+numTrees[i-j];
-    }
+    for(i = 1; i <= n; i++)
+        for(j = 0; j < i; j++)
+            numTrees[i] += numTrees[j]*numTrees[i-j-1];
 
+    return numTrees[n];
+}
+
+int main() {
+    assert(numTrees(3) == 5);
+
+    return 0;
 }
