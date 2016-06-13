adc8fc269f753a6d067f1e371bfde0ed7474f41a
hhmhywzr <eazow@163.com>
2016-06-13 12:01:15 +0800

    Update 108 Convert Sorted Array to Binary Search Tree

--- 108_convert_sorted_array_to_binary_search_tree.c
+++ 108_convert_sorted_array_to_binary_search_tree.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 
 struct TreeNode {
     int val;
@@ -6,15 +7,26 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-struct TreeNode* sortedArrayToBST(int* nums, numsSize) {
-    return sortedArrayToBSTRecursively(nums, 0, numsSize-1);
-}
-
 struct TreeNode* sortedArrayToBSTRecursively(int* nums, int low, int high) {
-    struct TreeNode* node = malloc(sizeof(struct TreeNode));
+    if(low > high) return NULL;
+    struct TreeNode* node = (struct TreeNode *)malloc(sizeof(struct TreeNode));
     int middle = (low+high)/2;
     node->val = nums[middle];
     node->left = sortedArrayToBSTRecursively(nums, low, middle-1);
-    node->right = sortedArrayToBSTRecursively(nums, middle, high+1);
+    node->right = sortedArrayToBSTRecursively(nums, middle+1, high);
     return node;
+}
+
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    return sortedArrayToBSTRecursively(nums, 0, numsSize-1);
+}
+
+int main() {
+    int nums[3] = {1,2,3};
+    struct TreeNode* root = sortedArrayToBST(nums, 3);
+    assert(root->val == 2);
+    assert(root->left->val == 1);
+    assert(root->right->val == 3);
+
+    return 0;
 }
