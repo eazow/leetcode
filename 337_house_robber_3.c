ff2c3ef16ebd461ec6f62bdb7742502459540fa8
hhmhywzr <eazow@163.com>
2016-06-16 18:06:35 +0800

    Add 337 House Robber III

---
+++ 337_house_robber_3.c
@@ -0,0 +1,45 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#define max(A, B) ((A)>(B)?(A):(B))
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+int* robRecursively(struct TreeNode* node) {
+    if(node == NULL)
+        return (int *)calloc(2, sizeof(int));
+    int* left = robRecursively(node->left);
+    int* right = robRecursively(node->right);
+    int* result = (int *)calloc(2, sizeof(int));
+    result[0] = node->val+left[1]+right[1];
+    result[1] = left[0] + right[0];
+    return result;
+}
+
+int rob(struct TreeNode* root) {
+    int* result = robRecursively(root);
+    return max(result[0], result[1]);
+}
+
+int main() {
+    struct TreeNode* root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    root->val = 5;
+    struct TreeNode* left = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    left->val = 1;
+    struct TreeNode* right = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    right->val = 2;
+    root->left = left;
+    root->right = right;
+    left->left = NULL;
+    left->right = NULL;
+    right->left = NULL;
+    right->right = NULL;
+
+    assert(rob(root) == 5);
+
+    return 0;
+}
