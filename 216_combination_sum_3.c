125b9eb3091e7b9b1dd81af47cb2ce258679e155
hhmhywzr <eazow@163.com>
2016-06-24 18:12:03 +0800

    Update 216 Combination Sum III

--- 216_combination_sum_3.c
+++ 216_combination_sum_3.c
@@ -1,4 +1,6 @@
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * Return an array of arrays of size *returnSize.
@@ -6,10 +8,38 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** combinationSum3(int k, int n, int** columnSizes, int* returnSize) {
+    int nums[6] = {0};
+
+    int num = 0;
+    int sum = 0;
+    int top = 0;
+    while(1) {
+        num++;
+        nums[top++] = num;
+        if(num > n && top==1)
+            return NULL;
+        sum += num;
+        if(sum == n && top == k) {
+            printf("1\n");
+            num = nums[--top];
+            sum -= num;
+        }
+        else if(top == k) {
+            num = nums[--top];
+            sum -= num;
+            while(num == n-1) {
+                num = nums[--top];
+                sum -= num;
+                if(top == 0 && num==n)
+                    return NULL;
+            }
+        }
+    }
 
 }
 
-int main() {
 
+int main() {
+    combinationSum3(3, 8, NULL, NULL);
     return 0;
 }
