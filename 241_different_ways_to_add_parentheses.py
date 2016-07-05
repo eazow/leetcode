3e9a8019559045e4ab98c73c88c9ac5177c53cd7
hhmhywzr <eazow@163.com>
2016-07-05 18:22:52 +0800

    Add 241 Different Ways to Add Parentheses

---
+++ 241_different_ways_to_add_parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def diffWaysToCompute(self, input):
+        if input.isdigit():
+            return [int(input)]
+        nums = []
+        for i, c in enumerate(input):
+            if c in '+-*':
+                nums1 = self.diffWaysToCompute(input[:i])
+                nums2 = self.diffWaysToCompute(input[i+1:])
+                for num1 in nums1:
+                    for num2 in nums2:
+                        if c == '*':
+                            nums.append(num1*num2)
+                        elif c == '+':
+                            nums.append(num1+num2)
+                        elif c == '-':
+                            nums.append(num1-num2)
+        return nums
+
+assert Solution().diffWaysToCompute("2-1-1")==[2,0]
+assert Solution().diffWaysToCompute("2*3-4*5")==[-34, -10, -14, -10, 10]
