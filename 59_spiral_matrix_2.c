e36f3613fbd8eda5db4474c18a878ceda2a4742b
hhmhywzr <eazow@163.com>
2016-07-05 11:33:00 +0800

    Update 59 Spiral Matrix II

--- 59_spiral_matrix_2.c
+++ 59_spiral_matrix_2.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 
 int** generateMatrix(int n) {
+    if(n == 0) return NULL;
+
     int** nums = (int**)malloc(sizeof(int *) * n);
     int i = 0;
     for(i = 0; i < n; i++) {
@@ -27,14 +29,22 @@ int** generateMatrix(int n) {
             nums[i--][j] = k++;
         step++;
     }
+    nums[i][j] = k;
 
     return nums;
 }
 
 int main() {
-    int** nums = generateMatrix(4);
-    printf("%d\n", nums[3][3]);
-
+    int** nums = generateMatrix(3);
+    assert(nums[0][0] == 1);
+    assert(nums[0][1] == 2);
+    assert(nums[0][2] == 3);
+    assert(nums[1][2] == 4);
+    assert(nums[2][2] == 5);
+    assert(nums[2][1] == 6);
+    assert(nums[2][0] == 7);
+    assert(nums[1][0] == 8);
+    assert(nums[1][1] == 9);
 
     return 0;
 }
