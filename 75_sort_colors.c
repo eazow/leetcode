319b8bd8987619d20fac751d4eefa7c511c643fe
hhmhywzr <eazow@163.com>
2016-07-14 02:08:22 +0800

    Update 75 Sort Colors

--- 75_sort_colors.c
+++ 75_sort_colors.c
@@ -1,5 +1,4 @@
 #include <assert.h>
-#include <stdio.h>
 
 void swap(int* a, int* b) {
     int temp = *a;
@@ -7,27 +6,39 @@ void swap(int* a, int* b) {
     *b = temp;
 }
 
-void printNums(int* nums, int numsSize) {
+int compareArrays(int* nums1, int* nums2, int numsSize) {
     int i = 0;
-    for(i = 0; i < 10; i++)
-        printf("%d ", nums[i]);
-    puts("");
+    for(i = 0; i < numsSize; i++) {
+        if(nums1[i] != nums2[i])
+            return 1;
+    }
+    return 0;
 }
 
 void sortColors(int* nums, int numsSize) {
     int i = 0, zeroIndex = 0, twoIndex = numsSize-1;
-    for(i = 0; i < twoIndex; i++) {
+    for(i = 0; i <= twoIndex; i++) {
         if(nums[i]==2)
-            swap(nums+i, nums+twoIndex--);
-        if(nums[i]==0 && i>zeroIndex)
-            swap(nums+i, nums+zeroIndex++);
-        printNums(nums, numsSize);
+            swap(nums+i--, nums+twoIndex--);
+        else if(nums[i]==0) {
+            if(i > zeroIndex)
+                swap(nums+i, nums+zeroIndex++);
+            else
+                zeroIndex++;
+        }
     }
 }
 
 int main() {
-    int nums[10] = {1,1,0,0,2,0,1,1,2,0};
-    sortColors(nums, 10);
+    int nums[3] = {0,1,0};
+    int sortedNums[3] = {0,0,1};
+    sortColors(nums, 3);
+    assert(compareArrays(nums, sortedNums, 3) == 0);
+
+    int nums2[6] = {1,2,0,0,2,1};
+    int sortedNums2[6] = {0,0,1,1,2,2};
+    sortColors(nums2, 6);
+    assert(compareArrays(nums2, sortedNums2, 6) == 0);
 
     return 0;
 }
