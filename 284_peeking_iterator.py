aaaccb0004e28820e86f317779686929c998a96c
hhmhywzr <eazow@163.com>
2016-07-17 23:59:09 +0800

    Add 284 Peeking Iterator

---
+++ 284_peeking_iterator.py
@@ -0,0 +1,12 @@
+class PeekingIterator(object):
+    def __init__(self, iterator):
+        pass
+
+    def peek(self):
+        pass
+
+    def next(self):
+        pass
+
+    def hasNext(self):
+        passs
