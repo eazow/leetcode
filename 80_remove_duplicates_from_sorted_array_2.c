9d77ed57557bc49c4927d4abbf2e559839f43962
hhmhywzr <eazow@163.com>
2016-07-22 18:48:21 +0800

    Add 80 Remove Duplicates from Sorted Array II

---
+++ 80_remove_duplicates_from_sorted_array_2.c
@@ -0,0 +1,9 @@
+#include <assert.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+    int pre = 0, cur = 0;
+    int count = 0;
+    for(pre = 0, cur = 0; cur < numsSize; cur++) {
+        
+    }
+}
