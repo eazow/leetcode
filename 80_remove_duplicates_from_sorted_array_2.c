7b428600775240f3683da426c8b55b9ac6d5e910
hhmhywzr <eazow@163.com>
2016-07-23 00:50:05 +0800

    Update 80 Remove Duplicates from Sorted Array II

--- 80_remove_duplicates_from_sorted_array_2.c
+++ 80_remove_duplicates_from_sorted_array_2.c
@@ -1,9 +1,23 @@
 #include <assert.h>
+#include <stdio.h>
 
 int removeDuplicates(int* nums, int numsSize) {
-    int pre = 0, cur = 0;
-    int count = 0;
-    for(pre = 0, cur = 0; cur < numsSize; cur++) {
-        
+    if(numsSize < 3)
+        return numsSize;
+    int i = 2;
+    int count = 2;
+    for(i = 2; i < numsSize; i++) {
+        if(nums[i] > nums[count-2])
+            nums[count++] = nums[i];
     }
+    return count;
+}
+
+int main() {
+    int nums[6] = {1,1,1,2,2,3};
+    assert(removeDuplicates(nums, 6) == 5);
+    assert(nums[2] == 2);
+    assert(nums[4] == 3);
+
+    return 0;
 }
