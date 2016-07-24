7cea663ec608554739bbdaf73c21aee1d42d7a40
hhmhywzr <eazow@163.com>
2016-07-24 23:52:39 +0800

    Update 129 Sum Root to Leaf Numbers

--- 129_sum_root_to_leaf_numbers.c
+++ 129_sum_root_to_leaf_numbers.c
@@ -1,5 +1,22 @@
 #include <assert.h>
 
-int sumNumbers(struct TreeNode* root) {
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
 
+int sumRecursively(struct TreeNode* node, int sum) {
+    if(node->left==NULL && node->right==NULL)
+        sum = sum*10+node->val;
+    return sum;
+}
+
+int sumNumbers(struct TreeNode* root) {
+    return sumRecursively(root, 0);
+}
+
+int main() {
+    
+    return 0;
 }
