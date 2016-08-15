da96a8ad9e46086554bf4137a1fe9cbd27b77ffc
hhmhywzr <eazow@163.com>
2016-08-15 22:56:40 +0800

    Add 39 Combination Sum

---
+++ 39_combination_sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type tatget: int
+        :rtype List[List[int]]
+        """
+
