2229b93173d7b60d146a683e4ac571f9be60dd57
hhmhywzr <eazow@163.com>
2016-08-16 21:43:32 +0800

    Update 39 Combination Sum

--- 39_combination_sum.py
+++ 39_combination_sum.py
@@ -5,4 +5,26 @@ class Solution(object):
         :type tatget: int
         :rtype List[List[int]]
         """
+        nums = []
+        returnLists = []
+        sum = 0
+        for i in range(len(candidates)):
+            while True:
+                if sum + candidates[i] > target:
+                    if len(nums) > 0:
+                        sum -= nums.pop()
+                    break
+                elif sum + candidates[i] == target:
+                    newNums = nums[:]
+                    newNums.append(candidates[i])
+                    returnLists.append(newNums)
+                    if len(nums) > 0:
+                        sum -= nums.pop()
+                    break
+                else:
+                    nums.append(candidates[i])
+                    sum += candidates[i]
 
+        return returnLists
+
+print Solution().combinationSum([2,3,4], 7)
