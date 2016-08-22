b9cdb19633efdc12a52ea2381deb9ee1ed8904b4
hhmhywzr <eazow@163.com>
2016-08-22 23:24:46 +0800

    Update 81 Search in Rotated Sorted Array II

--- 81_search_in_rotated_sorted_array_2.c
+++ 81_search_in_rotated_sorted_array_2.c
@@ -7,8 +7,26 @@ int search(int* nums, int numsSize, int target) {
         middle = left + (right-left)/2;
         if(nums[middle] == target)
             return 1;
-
+        if(nums[left] <= nums[right]) {
+            if(target > nums[middle])
+                left = middle+1;
+            else
+                right = middle-1;
+        }
+        else if(nums[left] <= nums[middle]) {
+            if(target > nums[middle] || target < nums[left])
+                left = middle + 1;
+            else
+                right = middle - 1;
+        }
+        else {
+            if(target > nums[middle] && target < nums[right])
+                left = middle + 1;
+            else 
+                right = middle - 1;
+        }
     }
+    return -1;
 }
 
 int main() {
