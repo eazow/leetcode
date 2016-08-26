53ce78f78890ef9874e9605ac5dc6183bae102a7
hhmhywzr <eazow@163.com>
2016-08-26 23:49:21 +0800

    Add 167 Two Sum II - Input array is sorted

---
+++ 167_two_sum_2_input_array_is_sorted.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numSize, int target, int* returnSize) {
+
+}
+
+int main() {
+
+
+    return 0;
+}
