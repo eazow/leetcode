7fe3ded409d94ce728dd5608a9d809a6bac001cd
hhmhywzr <eazow@163.com>
2016-09-02 23:58:02 +0800

    Add 114 Flatten Binary Tree to Linked List

---
+++ 114_flatten_binary_tree_to_linked_list.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+void flatten(struct TreeNode* root) {
+
+}
+
+int main() {
+
+    return 0;
+}
