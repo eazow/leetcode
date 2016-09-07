ede5563c32cd4b8ffd5298166ab162de1e78fe55
hhmhywzr <eazow@163.com>
2016-09-07 17:00:09 +0800

    Update 201 Bitwise AND of Numbers Range

--- 201_bitwise_and_of_numbers_range.c
+++ 201_bitwise_and_of_numbers_range.c
@@ -1,6 +1,17 @@
 #include <assert.h>
 
 int rangeBitwiseAnd(int m, int n) {
+    int bits = 1;
+    while(m != n) {
+        m >>= 1;
+        n >>= 1;
+        bits <<= 1;
+    }
+    return m*bits;
+}
+
+/**
+int rangeBitwiseAnd(int m, int n) {
     int bitwiseAnd = m;
     while(m <= n) {
         bitwiseAnd &= m;
@@ -8,9 +19,11 @@ int rangeBitwiseAnd(int m, int n) {
     }
     return bitwiseAnd;
 }
+*/
 
 int main() {
     assert(rangeBitwiseAnd(5,7) == 4);
+    assert(rangeBitwiseAnd(1,3) == 0);
 
     return 0;
 }
