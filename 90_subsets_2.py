ae9f951c0b6463073e0c941aac2d43224aadc3fd
hhmhywzr <eazow@163.com>
2016-09-10 23:43:21 +0800

    Add 90 Subsets II

---
+++ 90_subsets_2.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = [[]]
+        sort(nums)
