426a9c22ee11d35ab62c1ed6a9bb5e51479cd23e
hhmhywzr <eazow@163.com>
2016-09-21 02:09:23 +0800

    Update 90 Subsets II

--- 90_subsets_2.py
+++ 90_subsets_2.py
@@ -5,8 +5,21 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         result = [[]]
-        sort(nums)
+        nums.sort()
         i = 0
+        resultSize = 0
         while i < len(nums):
             num = nums[i]
-             
+            resultIndex = 0
+            if i>0 and nums[i]==nums[i-1]:
+                resultIndex = resultSize;
+            resultSize = len(result)
+            while resultIndex < resultSize:
+                tempList = result[resultIndex][:]
+                tempList.append(num)
+                result.append(tempList)
+                resultIndex += 1
+            i += 1
+        return result
+
+assert Solution().subsetsWithDup([1,2,2]) == [[],[1],[2],[1,2],[2,2],[1,2,2]]
