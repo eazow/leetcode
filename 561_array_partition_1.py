8e36702b227b64138d4c2dc3c73cf09855a5820c
huangyizhao <j-huangyizhao@360.cn>
2017-04-28 15:30:06 +0800

    Add 561 Array Partition I

---
+++ 561_array_partition_1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+    :type nums: List[int]
+    :rtype: int
+    """
+        nums.sort()
+        i = 0
+        sum = 0
+        while i < len(nums):
+            sum += nums[i]
+            i += 2
+        return sum
+
+assert Solution().arrayPairSum([1,4,3,2])==4
