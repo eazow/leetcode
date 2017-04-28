5124101bf401715fad0b0eb75b94d1f53dca0bfc
huangyizhao <j-huangyizhao@360.cn>
2017-04-28 14:53:14 +0800

    update 535 Encode and Decode TInyURL

--- 535_encode_and_decode_tinyurl.py
+++ 535_encode_and_decode_tinyurl.py
@@ -1,19 +1,30 @@
 class Codec:
+
+    def __init__(self):
+        self.map = {}
+        self.i = 0
     
     def encode(self, longUrl):
         """Encodes a URL to a shortened URL.
-
         :type longUrl: str
         :rtype: str
         """
+        self.i += 1
+        self.map[self.i] = longUrl
+        return "http://tinyurl.com/" + str(self.i)
+
 
     def decode(self, shortUrl):
         """Decodes a shortened URL to its original URL.
-
         :type shortUrl: str
         :rtype: str
         """
+        return self.map[int(shortUrl.split('/')[-1])]
 
 # Your Codes object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(url))
+
+url = "https://leetcode.com/problems/design-tinyurl";
+codec = Codec()
+assert codec.decode(codec.encode(url)) == url
