fba9d0a09392172d059d3127cd9549ecf37a2dc0
huangyizhao <j-huangyizhao@360.cn>
2017-05-05 12:10:20 +0800

    add 412 Fizz Buzz

---
+++ 412_fizz_buzz.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+
+        return result
+
+assert Solution().fizzBuzz(15) == [
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
