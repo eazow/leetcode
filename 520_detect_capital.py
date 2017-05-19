29e9be2f265a6c49c1e61c6bed3628bbe8a2c52b
eazow <eazow@163.com>
2017-05-19 14:40:44 +0800

    Add 520 Detect Capital

--- 520_detect_capital.py
+++ 520_detect_capital.py
@@ -1,5 +1,29 @@
 class Solution(object):
     def detectCapitalUse(self, word):
+        length = len(word)
+        if length == 0:
+            return True
+
+        firstIsCapital = word[0].isupper()
+        i = 1
+        hasLower = False
+        hasCapital = False
+        while i < length:
+            if word[i].islower():
+                hasLower = True
+            else:
+                hasCapital = True
+            if hasLower and hasCapital:
+                return False
+            i += 1
+
+        if not firstIsCapital and hasCapital:
+            return False
+
+        return True
+
+"""
+    def detectCapitalUse(self, word):
         capitalsCount = 0
         for i in range(len(word)):
             if word[i].isupper():
@@ -8,6 +32,10 @@ class Solution(object):
         if capitalsCount==0 or capitalsCount==len(word) or (capitalsCount==1 and word[0].isupper()):
             return True
         return False
+"""
 
 assert Solution().detectCapitalUse("USA") == True
 assert Solution().detectCapitalUse("FlaG") == False
+assert Solution().detectCapitalUse("ABc") == False
+assert Solution().detectCapitalUse("Leetcode") == True
+assert Solution().detectCapitalUse("aBC") == False
