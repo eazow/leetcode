74cec2be274ca9972018969d1879ee89c709ef6a
eazow <eazow@163.com>
2017-05-31 15:53:34 +0800

    Add 492 Construct the Rectangle

---
+++ 492_construct_the_rectangle.py
@@ -0,0 +1,14 @@
+import math
+
+class Solution(object):
+    def constructRectangle(self, area):
+        """
+        :type area: int
+        :rtype: List[int]
+        """
+        width = int(math.sqrt(area))
+        while area%width != 0:
+            width -= 1
+        return [area/width, width]
+
+Solution().constructRectangle(4) == [2,2]
