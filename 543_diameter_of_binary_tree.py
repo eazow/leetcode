e29e4ea2f8694e4eec4cea83b1008788bb573eeb
huangyizhao <eazow@163.com>
2017-08-28 17:10:11 +0800

    Update 543 Diameter of Binary Tree

--- 543_diameter_of_binary_tree.py
+++ 543_diameter_of_binary_tree.py
@@ -24,4 +24,4 @@ root.left = TreeNode(2)
 root.right = TreeNode(3)
 root.left.left = TreeNode(4)
 root.left.right = TreeNode(5)
-print Solution().diameterOfBinaryTree(root)
+assert Solution().diameterOfBinaryTree(root) == 3
