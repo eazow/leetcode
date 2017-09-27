f4bb87c6287adfb64a6a476ca2af3ee8725af7b4
eazow <eazow@163.com>
2017-09-27 12:45:15 +0800

    Update 594 Longest Harmonious Subsequence

--- 594_longest_harmonious_subsequence.py
+++ 594_longest_harmonious_subsequence.py
@@ -6,10 +6,7 @@ class Solution(object):
         """
         nums_count = {}
         for num in nums:
-            if nums_count.get(num):
-                nums_count[num] += 1
-            else:
-                nums_count[num] = 1
+            nums_count[num] = nums_count.get(num, 0) + 1
 
         result = 0
         for num in nums_count:
