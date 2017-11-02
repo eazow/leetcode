5810ee5f9dbb3d9a9c6ef66b73f6bd31e6cf5e88
huangyizhao <huangyizhao@b.360.cn>
2017-11-02 18:29:02 +0800

    Update 400 Nth Digit

--- 400_nth_digit.py
+++ 400_nth_digit.py
@@ -4,13 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        digits = ""
-        i = 1
-        while i <= n:
-            digits += str(i)
-            if len(digits) >= n:
-                return int(digits[n-1])
-            i += 1
+        bits = 1
+        count = 9
+        start_num = 1
 
-assert Solution().findNthDigit(3) == 3
+        while n > bits * count:
+            n -= bits * count
+            bits += 1
+            count *= 10
+            start_num *= 10
+
+        if n % bits == 0:
+            num = n / bits - 1 + start_num
+            return int(str(num)[-1])
+        else:
+            num = n / bits + start_num
+            return int(str(num)[n % bits - 1])
+
+
+assert Solution().findNthDigit(2) == 2
+assert Solution().findNthDigit(10) == 1
 assert Solution().findNthDigit(11) == 0
