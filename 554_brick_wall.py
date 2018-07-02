5d0acaff692f62e28eee7b504151f82543c9fed5
huangyizhao <huangyizhao@b.360.cn>
2018-07-02 16:16:29 +0800

    Update 554 Brick Wall

--- 554_brick_wall.py
+++ 554_brick_wall.py
@@ -4,24 +4,25 @@ class Solution(object):
         :type wall: List[List[int]]
         :rtype: int
         """
-        bricks_map = {}
+        empty_count_map = {}
         for row in wall:
             sum = 0
             col = 0
             for brick in row:
                 sum += brick
-                if sum in bricks_map:
-                    bricks_map[sum] += 1
+                if sum in empty_count_map:
+                    empty_count_map[sum] += 1
                 elif col != len(row) - 1:
-                    bricks_map[sum] = 1
+                    empty_count_map[sum] = 1
                 col += 1
 
-        height = len(wall)
-        least = height
-        for brick in bricks_map:
-            least = min(height - bricks_map.get(brick), least)
+        max_count = 0
+        for empty in empty_count_map:
+            count = empty_count_map.get(empty)
+            max_count = max(max_count, count)
 
-        return least
+        return len(wall) - max_count
+
 
 assert Solution().leastBricks([
     [1,2,2,1],
