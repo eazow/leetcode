e8ef3ca3807424cbc0e34da40b17541aed8fb8a4
huangyizhao <huangyizhao@b.360.cn>
2018-08-03 17:48:59 +0800

    Update 15 3Sum

--- 15_3sum.py
+++ 15_3sum.py
@@ -6,6 +6,9 @@ class Solution(object):
         """
         result = {}
         nums = sorted(nums)
+
+        """
+        Time Limit Exceeded
         for i in range(len(nums)):
             j = len(nums) - 1
             while j > i + 1:
@@ -23,8 +26,26 @@ class Solution(object):
                         k_end = k - 1
                 j -= 1
         return result.values()
+        """
+
+        for i in range(len(nums) - 2):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                the_sum = nums[i] + nums[j] + nums[k]
+                if the_sum == 0:
+                    result[nums[i], nums[k], nums[j]] = [nums[i], nums[j], nums[k]]
+                    if nums[j] == nums[k]:
+                        break
+                    k -= 1
+                    j += 1
+                elif the_sum > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return result.values()
 
 
-print Solution().threeSum([-1, 0, 1, 2, -1, -4])
-print Solution().threeSum([1, 2, -2, -1])
-print Solution().threeSum([-2, 0, 1, 1, 2])
+assert Solution().threeSum([-1, 0, 1, 2, -1, -4]) == [[-1,0,1], [-1, -1, 2]]
+assert Solution().threeSum([1, 2, -2, -1]) == []
+assert Solution().threeSum([-2, 0, 1, 1, 2]) == [[-2, 0, 2], [-2, 1, 1]]
