e7ced4d36699baf32a7a7b8b50e421dc84428687
huangyizhao <huangyizhao@b.360.cn>
2018-09-10 19:59:07 +0800

    Update 23 Merge k Sorted Lists

--- 23_merge_k_sorted_list.py
+++ 23_merge_k_sorted_list.py
@@ -10,31 +10,20 @@ class Solution(object):
         :param lists: List[ListNode]
         :return: ListNode
         """
+        nums = []
+        for list_node in lists:
+            current = list_node
+            while current:
+                nums.append(current.val)
+                current = current.next
+
+        nums.sort()
+
         head = ListNode(0)
         tail = head
-
-        while True:
-            min_value = float("inf")
-            min_index = -1
-
-            for i in range(len(lists)):
-                list_node = lists[i]
-                if list_node:
-                    all_nodes_are_empty = False
-                    if min_index == -1:
-                        min_index = i
-                        min_value = list_node.val
-                    elif list_node.val < min_value:
-                        min_value = list_node.val
-                        min_index = i
-
-            if min_index == -1:
-                break
-
-            tail.next = ListNode(min_value)
+        for num in nums:
+            tail.next = ListNode(num)
             tail = tail.next
-
-            lists[min_index] = lists[min_index].next
 
         return head.next
 
@@ -50,9 +39,12 @@ list2.next.next = ListNode(4)
 list3 = ListNode(2)
 list3.next = ListNode(6)
 
-
-merge_list = Solution().mergeKLists([list1, list2, list3])
-node = merge_list
-while node:
-    print node.val
-    node = node.next
+head_node = Solution().mergeKLists([list1, list2, list3])
+assert head_node.val == 1
+assert head_node.next.val == 1
+assert head_node.next.next.val == 2
+assert head_node.next.next.next.val == 3
+assert head_node.next.next.next.next.val == 4
+assert head_node.next.next.next.next.next.val == 4
+assert head_node.next.next.next.next.next.next.val == 5
+assert head_node.next.next.next.next.next.next.next.val == 6
