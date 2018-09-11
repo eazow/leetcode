afde002f6c390e98265d7891ab3ee3b8c511bb5d
huangyizhao <huangyizhao@b.360.cn>
2018-09-11 18:21:41 +0800

    Add 29 Divide Two Integers

---
+++ 29_divide_two_integers.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :param dividend: int
+        :param divisor: int
+        :return: int
+        """
+        return int(dividend / divisor)
