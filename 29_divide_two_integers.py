24077f68ce3a66518bfb875bf8c0edd7d3aef0ba
huangyizhao <huangyizhao@b.360.cn>
2018-09-12 14:38:02 +0800

    Update 29 Divide Two Integers

--- 29_divide_two_integers.py
+++ 29_divide_two_integers.py
@@ -1,3 +1,7 @@
+MIN_INTEGER = -1 * (1 << 31)
+MAX_INTEGER = (1 << 31) - 1
+
+
 class Solution(object):
     def divide(self, dividend, divisor):
         """
@@ -5,4 +9,13 @@ class Solution(object):
         :param divisor: int
         :return: int
         """
-        return int(dividend / divisor)
+        result = int(dividend / float(divisor))
+        if result > MAX_INTEGER:
+            result = MAX_INTEGER
+        # elif result < MIN_INTEGER:
+        #     result = MIN_INTEGER
+        return result
+
+
+assert Solution().divide(-2147483648, -1) == 2147483647
+assert Solution().divide(10, 3) == 3
