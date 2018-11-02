df53866e154f1a0cc6abdca2c6bd4ef11bb5775e
huangyizhao <huangyizhao@b.360.cn>
2018-11-02 19:44:45 +0800

    Update 40 Combination Sum II

--- 40_combination_sum_2.py
+++ 40_combination_sum_2.py
@@ -1,6 +1,12 @@
 # -*- coding:utf-8 -*-
+import copy
+
 
 class Solution(object):
+    def __init__(self):
+        self.result = []
+        self.result_set = set()
+
     def combinationSum2(self, candidates, target):
         """
         :param candidates: List[int]
@@ -8,21 +14,24 @@ class Solution(object):
         :return: List[List[int]]
         """
         candidates.sort()
-        self.result = []
-        self.traverse(candidates, target)
+        self.traverse(candidates, target, [])
+        return self.result
 
-    def traverse(self, candidates, target):
+    def traverse(self, candidates, target, nums):
         if target == 0:
-            print self.result
-            self.result = []
+            nums_token = "_".join(map(str, nums))
+            if nums_token not in self.result_set:
+                self.result.append(nums)
+                self.result_set.add(nums_token)
 
         elif target > 0:
             for i in range(len(candidates)):
-                self.result.append(candidates[i])
-                print candidates[0:i] + candidates[i + 1:len(candidates)], target - candidates[i]
-                self.traverse(candidates[0:i] + candidates[i + 1:len(candidates)], target - candidates[i])
-        else:
-            self.result = []
+                if target >= candidates[i]:
+                    traverse_nums = copy.deepcopy(nums)
+                    traverse_nums.append(candidates[i])
+                    self.traverse(candidates[i + 1:len(candidates)], target - candidates[i], traverse_nums)
+                else:
+                    break
 
 
 Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
