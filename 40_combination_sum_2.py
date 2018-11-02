c382d85be7d23e64aa6345345e2792e8e8789315
huangyizhao <huangyizhao@b.360.cn>
2018-11-02 19:47:38 +0800

    Add 34 Find First and Last Position of Element in Sorted Array

--- 40_combination_sum_2.py
+++ 40_combination_sum_2.py
@@ -34,4 +34,4 @@ class Solution(object):
                     break
 
 
-Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8)
+assert Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8) == [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]
