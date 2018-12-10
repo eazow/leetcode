73a4964ca76bbea84b438e4850c0047567bac7c4
huangyizhao <huangyizhao@b.360.cn>
2018-12-10 15:39:51 +0800

    Update 50 Pow(x, n)

--- 50_pow(x,n).py
+++ 50_pow(x,n).py
@@ -5,31 +5,19 @@ class Solution(object):
         :param n: int
         :return: float
         """
-        negative = False
-
         if n == 0:
             return 1
+        elif n == 1:
+            return x
         elif n < 0:
-            negative = True
             n = -1 * n
+            x = 1.0 / x
 
-        i = 1
-        result = x
-        while i <= n:
-            if i + i <= n:
-                result *= result
-                i += i
-            else:
-                result *= x
-                i += 1
-
-        if negative:
-            result = 1 / float(result)
-
+        result = self.myPow(x * x, n/2) if n % 2 == 0 else x * self.myPow(x, n - 1)
         return result
 
 
-print Solution().myPow(2, 10)
-print Solution().myPow(2.1, 3)
-print Solution().myPow(2, -2)
-print Solution().myPow(0.00001, 2147483647)
+assert Solution().myPow(2, 10) == 1024
+assert round(Solution().myPow(2.1, 3), 3) == 9.261
+assert Solution().myPow(2, -2) == 0.25
+assert Solution().myPow(0.00001, 2147483647) == 0
