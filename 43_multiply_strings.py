5f5426072d55c25c9295cbe50815fc233d8876fa
huangyizhao <huangyizhao@b.360.cn>
2018-12-14 16:41:47 +0800

    Update 43 Multiply Strings

--- 43_multiply_strings.py
+++ 43_multiply_strings.py
@@ -5,7 +5,18 @@ class Solution(object):
         :param num2: str
         :return: str
         """
-        return str(int(num1) * int(num2))
+        # return str(int(num1) * int(num2))
+
+        result = 0
+        for i in xrange(len(num1)-1, -1, -1):
+            for j in xrange(len(num2)-1, -1, -1):
+                base = 1
+                for k in xrange((len(num1) - 1 - i) + (len(num2) - 1 - j)):
+                    base *= 10
+                result += int(num1[i]) * int(num2[j]) * base
+
+        return str(result)
 
 
-assert Solution().multiply("2", "3") == "6"
+# assert Solution().multiply("2", "3") == "6"
+print Solution().multiply("123", "456")
