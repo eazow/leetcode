aaf49f9a8ae26d7db9b4de7e3bce142a3a359d8f
huangyizhao <huangyizhao@b.360.cn>
2018-12-17 15:06:06 +0800

    Update 43 Multiply Strings

--- 43_multiply_strings.py
+++ 43_multiply_strings.py
@@ -7,16 +7,29 @@ class Solution(object):
         """
         # return str(int(num1) * int(num2))
 
-        result = 0
-        for i in xrange(len(num1)-1, -1, -1):
-            for j in xrange(len(num2)-1, -1, -1):
-                base = 1
-                for k in xrange((len(num1) - 1 - i) + (len(num2) - 1 - j)):
-                    base *= 10
-                result += int(num1[i]) * int(num2[j]) * base
+        if num1 == "0" or num2 == "0":
+            return "0"
 
-        return str(result)
+        result = [0] * (len(num1) + len(num2))
+
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        for i in xrange(len(num1)):
+            for j in xrange(len(num2)):
+                result[i+j] += int(num1[i]) * int(num2[j])
+
+                if result[i+j] >= 10:
+                    result[i+j+1] += result[i+j] / 10
+                    result[i+j] = result[i+j] % 10
+
+        result = result[::-1]
+        i = 0
+        while result[i] == 0:
+            i += 1
+
+        return "".join(map(str, result[i:]))
 
 
-# assert Solution().multiply("2", "3") == "6"
-print Solution().multiply("123", "456")
+assert Solution().multiply("2", "3") == "6"
+assert Solution().multiply("0", "0") == "0"
+assert Solution().multiply("123", "456") == "56088"
