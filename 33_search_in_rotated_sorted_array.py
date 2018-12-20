d745db65d41c772bb363bb392aef8ec8e9a875aa
huangyizhao <huangyizhao@b.360.cn>
2018-12-20 15:35:49 +0800

    Update 33 Search in Rotated Sorted Array

--- 33_search_in_rotated_sorted_array.py
+++ 33_search_in_rotated_sorted_array.py
@@ -13,27 +13,24 @@ class Solution(object):
             if nums[middle] == target:
                 return middle
 
-            # [4, 5, 6, 7, 0, 1, 2]
-            if nums[middle] > nums[low]:
-                if nums[low] < target < nums[middle]:
-                    high = middle
-                elif target > nums[high]:
-                    return -1
+            # 左半段有序, 类似[4, 5, 6, 7, 8, 0, 1, 2]，注意需要加上nums[middle] == nums[low]的条件
+            if nums[middle] >= nums[low]:
+                if nums[low] <= target < nums[middle]:
+                    high = middle - 1
                 else:
                     low = middle + 1
-            # [4, 5, 6, 0, 1, 2, 3]
+            # 右半段有序，类似[4, 5, 6, 0, 1, 2, 3]
             else:
-                if nums[middle] < target < nums[high]:
+                if nums[middle] < target <= nums[high]:
                     low = middle + 1
-                elif target < nums[low]:
-                    return -1
                 else:
                     high = middle - 1
 
         return -1
 
 
-
-print Solution().search([1, 2, 3, 4, 5, 6, 7], 1)
+# print Solution().search([1, 2, 3, 4, 5, 6, 7], 1)
+# print Solution().search([4, 5, 6, 7, 0, 1, 2], 0)
+print Solution().search([5, 1, 2, 3, 4], 1)
 
 # print Solution().search([4, 5, 6, 7, 0, 1, 2], 0)
