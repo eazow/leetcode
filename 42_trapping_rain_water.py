d5b5eb2b921a416cc2efae1ccd3855428307cdc7
huangyizhao <huangyizhao@b.360.cn>
2018-12-25 19:15:36 +0800

    Update 42 Trapping Rain Water

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -6,3 +6,30 @@ class Solution(object):
         :param height: List[int]
         :return: int
         """
+        result = 0
+        max_height = 0
+        max_height_index = 0
+        for i in xrange(len(height)):
+            if height[i] > max_height:
+                max_height = height[i]
+                max_height_index = i
+
+        left_max = 0
+        for i in xrange(0, max_height_index):
+            if height[i] > left_max:
+                left_max = height[i]
+            else:
+                result += left_max - height[i]
+
+        right_max = 0
+        for i in xrange(len(height)-1, max_height_index, -1):
+            if height[i] > right_max:
+                right_max = height[i]
+            else:
+                result += right_max - height[i]
+
+        return result
+
+
+assert Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6
+
