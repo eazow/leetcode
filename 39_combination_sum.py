7b89f8f7389b5ad02864b32263cceaddd32d276d
eazow <eazow@163.com>
2019-04-23 14:36:25 +0800

    Update 39 Combination Sum

--- 39_combination_sum.py
+++ 39_combination_sum.py
@@ -1,43 +1,48 @@
 class Solution(object):
+    # def combinationSum(self, candidates, target):
+    #     """
+    #     :type candidates: List[int]
+    #     :type tatget: int
+    #     :rtype List[List[int]]
+    #     """
+    #     nums = []
+    #     indexes = []
+    #     returnLists = []
+    #     sum = 0
+    #     i = 0
+    #     candidates.sort()
+    #     while True:
+    #         while i >= len(candidates):
+    #             if len(nums) == 0:
+    #                 return returnLists
+    #             sum -= nums.pop()
+    #             i = indexes.pop()+1
+    #         if sum + candidates[i] < target:
+    #             nums.append(candidates[i])
+    #             indexes.append(i)
+    #             sum += candidates[i]
+    #         elif sum + candidates[i] > target:
+    #             if len(nums) > 0:
+    #                 sum -= nums.pop()
+    #                 i = indexes.pop()+1
+    #             else:
+    #                 return returnLists
+    #         elif sum + candidates[i] == target:
+    #             newNums = nums[:]
+    #             newNums.append(candidates[i])
+    #             returnLists.append(newNums)
+    #             if len(nums) > 0:
+    #                 sum -= nums.pop()
+    #                 i = indexes.pop()+1
+    #             else:
+    #                 i += 1
+
     def combinationSum(self, candidates, target):
         """
         :type candidates: List[int]
         :type tatget: int
         :rtype List[List[int]]
         """
-        nums = []
-        indexes = []
-        returnLists = []
-        sum = 0
-        i = 0
-        candidates.sort()
-        while True:
-            while i >= len(candidates):
-                if len(nums) == 0:
-                    return returnLists
-                sum -= nums.pop()
-                i = indexes.pop()+1
-            if sum + candidates[i] < target:
-                nums.append(candidates[i])
-                indexes.append(i)
-                sum += candidates[i]
-            elif sum + candidates[i] > target:
-                if len(nums) > 0:
-                    sum -= nums.pop()
-                    i = indexes.pop()+1
-                else:
-                    return returnLists
-            elif sum + candidates[i] == target:
-                newNums = nums[:]
-                newNums.append(candidates[i])
-                returnLists.append(newNums)
-                if len(nums) > 0:
-                    sum -= nums.pop()
-                    i = indexes.pop()+1
-                else:
-                    i += 1
-
-    def combination_sum(self, candidates, target):
         self.result_nums = []
         candidates.sort()
         self.dfs(candidates, 0, target, [])
@@ -48,21 +53,18 @@ class Solution(object):
         while i < len(nums):
             num = nums[i]
             if num == target:
-                self.result_nums.append(temp_nums.append(num))
-                print self.result_nums
-                break
+                temp_nums.append(num)
+                self.result_nums.append(temp_nums)
+                return
             elif num < target:
                 temp_nums.append(num)
                 self.dfs(nums, i, target-num, temp_nums[:])
+                temp_nums.pop()
             else:
                 return
             i += 1
 
 
-
-# assert Solution().combinationSum([2, 3, 4], 7) == [[2, 2, 3], [3, 4]]
-# assert Solution().combinationSum([2, 3, 6, 7], 7) == [[2, 2, 3], [7]]
-# assert Solution().combinationSum([2], 1) == []
-
-
-print Solution().combination_sum([2, 3, 4], 7)
+assert Solution().combinationSum([2, 3, 4], 7) == [[2, 2, 3], [3, 4]]
+assert Solution().combinationSum([2, 3, 6, 7], 7) == [[2, 2, 3], [7]]
+assert Solution().combinationSum([2], 1) == []
