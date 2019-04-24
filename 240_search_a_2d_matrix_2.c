02b300620056a1ebca259854d819467b48647e60
eazow <eazow@163.com>
2019-04-24 16:35:51 +0800

    Update 240 Search a 2D Matrix II

--- 240_search_a_2d_matrix_2.c
+++ 240_search_a_2d_matrix_2.c
@@ -3,8 +3,8 @@
 
 int searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
     //search from top-right to left-bottom
-    int i = 0, j = matrixColSize-1;
-    while(i<matrixRowSize && j>=0) {
+    int i = 0, j = matrixColSize - 1;
+    while(i < matrixRowSize && j >= 0) {
         if(target > matrix[i][j])
             i++;
         else if(target < matrix[i][j])
@@ -16,11 +16,11 @@ int searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target)
 }
 
 int main() {
-    int nums1[] = {1,4,7,11,15};
-    int nums2[] = {2,5,8,12,19};
-    int nums3[] = {3,6,9,16,22};
-    int nums4[] = {10,13,14,17,24};
-    int nums5[] = {18,21,23,26,30};
+    int nums1[] = {1, 4, 7, 11, 15};
+    int nums2[] = {2, 5, 8, 12, 19};
+    int nums3[] = {3, 6, 9, 16, 22};
+    int nums4[] = {10, 13, 14, 17, 24};
+    int nums5[] = {18, 21, 23, 26, 30};
     int **matrix = (int **)malloc(sizeof(int *)*5);
     matrix[0] = nums1;
     matrix[1] = nums2;
